f066cd9418cf07fecdaa59a539ec1ccf3ba3485f
jagadeeswaran <suryajagadish7@gmail.com>
2022-02-22 15:27:04 +0530

    add image upload feature with multer

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer');
 
 const fileRouter = require('./routes/file.router');
 const authRouter = require('./routes/auth.router');
+const req = require('express/lib/request');
+
+const storage = multer.diskStorage({
+    destination: (request, file, callback) => {
+        callback(null, './public/images');
+    },
+    filename: (request, file, callback) => {
+        const fileName = `${file.fieldname}-${new Date().getUTCMilliseconds()}-${Math.floor(Math.random() * 10000000)}.jpeg`;
+        callback(null, fileName);
+    }
+})
+
+const upload = multer({ storage });
 
 const app = express();
 
+app.use(express.static(path.join(process.env.PWD, 'public')));
 app.use(bodyParser.json());
 
 app.use('/file', fileRouter);
 app.use('/auth', authRouter);
+
+app.post('/upload', upload.single('image'), (request, response, next) => {
+    response.json({
+        action: "success",
+        filename: request.file.filename
+    });
+})
 
 app.use((request, response, next) => {
     response.status(404).json({
